3a9816f488786c33d98eea8e6f2bcf4192a4cc85
abbas ejlali <abbas.ejlali18@gmail.com>
2022-12-11 09:34:33 -0800

    add all jsx in singup component

--- src/Components/Singup.js
+++ src/Components/Singup.js
@@ -17,6 +17,22 @@ const Singup = () => {
           <label>Name</label>
           <input type="text" name="name" />
         </div>
+        <div>
+          <label>Email</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" name="Password" />
+        </div>
+        <div>
+          <label>Confing Password</label>
+          <input type="password" name="confingpass" />
+        </div>
+        <div>
+          <label>Accepted all anyone!?</label>
+          <input type="checkbox" name="isaccepted" />
+        </div>
       </form>
     </div>
   );
