e49dad223639b6020194ddac666eaa70da0caa92
abbas ejlali <abbas.ejlali18@gmail.com>
2022-12-11 11:02:41 -0800

    create validation coponent and add valid function

---
+++ src/Components/Validation.js
@@ -0,0 +1,39 @@
+export const valid = (data) => {
+  const errors = {};
+
+  if (!data.name) {
+    errors.name = "name required";
+  } else {
+    delete errors.name;
+  }
+
+  if (!data.email) {
+    errors.email = "email required";
+  } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+    errors.email = "email not valide";
+  } else {
+    delete errors.email;
+  }
+
+  if (!data.password) {
+    errors.password = "password required";
+  } else if (data.password.lenghth < 6) {
+    errors.password = "The entered value is less than 6";
+  } else {
+    delete errors.password;
+  }
+
+  if (!data.confingpass) {
+    data.confingpass = "confingpass required";
+  } else if (data.confingpass !== data.password) {
+    data.confingpass = "The entered values ​​are not equal";
+  } else {
+    delete errors.confingpass;
+  }
+
+  if (data.isaccepted) {
+    delete data.isaccepted;
+  } else {
+    data.isaccepted = "isaccepted required";
+  }
+};
