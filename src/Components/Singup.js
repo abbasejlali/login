0761ec4417b9ee033ac209c76cf93538a535eda1
abbas ejlali <abbas.ejlali18@gmail.com>
2022-12-11 09:50:33 -0800

    betting using useState to forms

--- src/Components/Singup.js
+++ src/Components/Singup.js
@@ -9,29 +9,62 @@ const Singup = () => {
     isaccepted: false,
   });
 
+  const changesHandeler = (e) => {
+    if (e.target.name === "isaccepted") {
+      setdata({ ...data, [e.target.name]: e.target.checked });
+    } else {
+      setdata({ ...data, [e.target.name]: e.target.value });
+    }
+    console.log(data);
+  };
   return (
     <div>
       <h1>Singup page</h1>
       <form>
         <div>
           <label>Name</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            value={data.name}
+            onChange={changesHandeler}
+          />
         </div>
         <div>
           <label>Email</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={data.email}
+            onChange={changesHandeler}
+          />
         </div>
         <div>
           <label>Password</label>
-          <input type="password" name="Password" />
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={changesHandeler}
+          />
         </div>
         <div>
           <label>Confing Password</label>
-          <input type="password" name="confingpass" />
+          <input
+            type="password"
+            name="confingpass"
+            value={data.confingpass}
+            onChange={changesHandeler}
+          />
         </div>
         <div>
           <label>Accepted all anyone!?</label>
-          <input type="checkbox" name="isaccepted" />
+          <input
+            type="checkbox"
+            name="isaccepted"
+            value={data.isaccepted}
+            onChange={changesHandeler}
+          />
         </div>
       </form>
     </div>
