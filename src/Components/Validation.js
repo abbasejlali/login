e947e05c4f72e84c3b8054d1e6ef8abba0736af2
abbas ejlali <abbas.ejlali18@gmail.com>
2022-12-12 07:41:01 -0800

    add jsx to main project and edit validation component

---
+++ src/Components/Validation.js
@@ -0,0 +1,43 @@
+const validation = (data) => {
+  const errors = {};
+
+  if (!data.name) {
+    errors.name = "name required";
+  } else {
+    delete errors.name;
+  }
+
+  if (!data.email) {
+    errors.email = "email required";
+  } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+    errors.email = "email not valide";
+  } else {
+    delete errors.email;
+  }
+
+  if (!data.password) {
+    errors.password = "password required";
+  } else if (data.password.length < 6) {
+    errors.password = "The entered value is less than 6";
+  } else {
+    delete errors.password;
+  }
+
+  if (!data.confingpass) {
+    data.confingpass = "confingpass required";
+  } else if (data.confingpass !== data.password) {
+    data.confingpass = "The entered values ​​are not equal";
+  } else {
+    delete errors.confingpass;
+  }
+
+  if (data.isaccepted) {
+    delete data.isaccepted;
+  } else {
+    data.isaccepted = "isaccepted required";
+  }
+
+  return errors;
+};
+
+export default validation;
